b9c795212d48b1dd52627e92c014a117addcdec7
Joe <jjiang2@deloitte.com.au>
2018-11-08 22:53:32 +1100

    Add MCQ as interactive component

--- main.py
+++ main.py
@@ -15,7 +15,7 @@
 # [START gae_python37_app]
 from flask import Flask
 from flask import Response
-from flask import Request
+from flask import request
 import simplejson as json
 
 
@@ -32,7 +32,7 @@ def test():
         'number' : 3,
         'response_type' : 'in_channel',
         'text': 'hello from xy',
-        'attachments': 
+        'attachments':
         [
             {
                 'text': 'There is no attachments'
@@ -50,23 +50,125 @@ def test():
 
 
 @app.route('/hello', methods=['POST'])
-def simple_response():
+def return_quiz():
     """Return a simple response"""
-    data = {
-        'response_type': 'in_channel',
-        'text': 'It is a simple resonse',
-        'attachments': [
-        {
-            'text': 'There is no attachments'
+    command_text = request.args.get('text')
+
+    if (command_text.lower() == 'quiz'):
+        data = {
+            'text': 'Okay, let`s do this mcq qustion.\nWhat is Alber Einstein`s birthday\nA: 14 March 1879\nB:15 April 1880\nC:16 May 1881\nD:17 June 1882',
+            'fallback': 'You are unable to make the choice',
+            'callback_id': 'einstein_birthday',
+            'color': '#3AA3E3',
+            'attachment_type': 'default',
+            'actions': [
+                {
+                    'name': 'birthday',
+                    'text': 'A',
+                    'type': 'button',
+                    'value': 'A',
+                    'confirm': {
+                        'title': 'Are you sure?',
+                        'text': 'Would you prefer another date',
+                        'ok_text': 'Yes',
+                        'dismiss_text': 'No'
+                    }
+                },
+                {
+                    'name': 'birthday',
+                    'text': 'B',
+                    'type': 'button',
+                    'value': 'B',
+                    'confirm': {
+                        'title': 'Are you sure?',
+                        'text': 'Would you prefer another date',
+                        'ok_text': 'Yes',
+                        'dismiss_text': 'No'
+                    }
+                },
+                {
+                    'name': 'birthday',
+                    'text': 'C',
+                    'type': 'button',
+                    'value': 'C',
+                    'confirm': {
+                        'title': 'Are you sure?',
+                        'text': 'Would you prefer another date',
+                        'ok_text': 'Yes',
+                        'dismiss_text': 'No'
+                    }
+                },
+                {
+                    'name': 'birthday',
+                    'text': 'D',
+                    'type': 'button',
+                    'value': 'D',
+                    'confirm': {
+                        'title': 'Are you sure?',
+                        'text': 'Would you prefer another date',
+                        'ok_text': 'Yes',
+                        'dismiss_text': 'No'
+                    }
+                }
+                ]
+            }
+    else:
+        data = {
+            'response_type': 'in_channel',
+            'text': 'Please type quiz',
+            'attachments': [
+                {
+                    'text': 'This is a reminder message'
+                }
+            ]
         }
-        ]
-    }
 
     js = json.dumps(data)
     resp = Response(js,status=200,mimetype='application/json')
     resp.headers['Content-type'] = 'application/json'
-
     return resp
+
+@app.route('/hello/response', methods=['POST'])
+def check_answer():
+    json_data=request.get_json()
+    if(json_data['type'].lower() == 'interactive_message'):
+        if(json_data['actions'][0]['value'].lower() == 'a'):
+            data = {
+                'response_type': 'in_channel',
+                'text': 'Congrats! You have got it right',
+                'attachments': [
+                    {
+                        'text': 'This is an correct answer'
+                    }
+                ]
+            }
+        else:
+            data = {
+                'response_type': 'in_channel',
+                'text': 'Sorry! You have got it wrong',
+                'attachments': [
+                    {
+                        'text': 'This is an wrong answer'
+                    }
+                ]
+            }
+    else:
+        data = {
+            'response_type': 'in_channel',
+            'text': 'We did not get your message',
+            'attachments': [
+                {
+                    'text': 'This is an error message'
+                }
+            ]
+        }
+
+    js = json.dumps(data)
+    resp = Response(js,status=200,mimetype='application/json')
+    resp.headers['Content-type'] = 'application/json'
+    return resp
+
+
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
